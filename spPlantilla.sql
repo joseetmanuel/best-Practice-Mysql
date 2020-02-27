f6c34ab32fca12c62feef696ecc57e9305d3891f
Jose Etmanuel Hernandez Rejon <joseetmanuel@gmail.com>
2020-02-27 10:43:55 -0600

    sql_beatiful

--- spPlantilla.sql
+++ spPlantilla.sql
@@ -7,23 +7,23 @@ CREATE DEFINER=`root`@`%` PROCEDURE `action_tabla_sp`(
 
 )
 BEGIN
-	# variables 
+    # variables 
    
 	
     # manejo de errores
-	DECLARE exit handler for SQLEXCEPTION
+    DECLARE exit handler for SQLEXCEPTION
     BEGIN
-		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
-		SET @full_error = CONCAT("ERROR ", @errno, " (", @sqlstate, "): ", @text);
-		SELECT @full_error as err;
-		ROLLBACK;
-	END;
-	SET autocommit=0;
+	GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
+	SET @full_error = CONCAT("ERROR ", @errno, " (", @sqlstate, "): ", @text);
+	SELECT @full_error as err;
+	ROLLBACK;
+    END;
+    SET autocommit=0;
 	
     # insersión de datos
-	START TRANSACTION;
+    START TRANSACTION;
 		
         
-	COMMIT;  
+    COMMIT;  
 END$$
 DELIMITER ;	
